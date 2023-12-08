76c53a7c498e4cd0cadbb28260a9ccd0691b90a4
nathan short <nathan@example.com>
2023-12-08 06:38:40 -0500

    day 08

--- 08/run.rb
+++ 08/run.rb
@@ -1,4 +1,13 @@
 
+def do_count( ins, map, parent, end_with )
+  count = 0
+  ins.cycle do |i| 
+    count += 1
+    parent = map[parent][i]
+    return count if parent.end_with?( end_with )
+  end
+end
+
 a,b = ARGF.read.split(/\n\n/)
 ins = a.chars
 map = {}
@@ -6,30 +15,8 @@ b.split(/\n/).map do |x|
   parent, l, r = x.scan(/[A-Z]+/)
   map[parent] = {'L'=>l, 'R'=>r}
 end
+p do_count( ins, map, 'AAA', 'ZZZ')
 
-parent = 'AAA'
-count = 0
-ins.cycle do |i| 
-  count += 1
-  parent = map[parent][i]
-  break if parent == 'ZZZ'
-end
-
-p count
-
-pmap = {}
-map.keys.select{ |k| k[2] == 'A' }.each do |pp|
-  parent = pp
-  count = 0
-  ins.cycle do |i| 
-    count += 1
-    parent = map[parent][i]
-    if parent[2] == 'Z'
-      pmap[pp] = count
-      break
-    end
-  end
-end
-
-p pmap.values.reduce(1) { |a,v| a.lcm(v) }
+counts =  map.keys.select{ |k| k[2] == 'A' }.map { |pp| do_count( ins, map, pp, 'Z') }
+p counts.reduce(1) { |a,v| a.lcm(v) }
 
